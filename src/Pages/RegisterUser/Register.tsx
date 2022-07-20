23316755529bf2f44d29e11676153fe6ae787c20
ashikmahmud <ashikmamud187@gmail.com>
2022-07-21 02:50:08 +0600

    create function to taking input value and many more

--- src/Pages/RegisterUser/Register.tsx
+++ src/Pages/RegisterUser/Register.tsx
@@ -1,10 +1,60 @@
+import { useState } from "react";
+import { toast } from "react-hot-toast";
 import { MdOutlineArrowBackIos } from "react-icons/md";
 import { Link, useNavigate } from "react-router-dom";
 const Register = () => {
   const navigate = useNavigate();
+
+  /* form types */
+  type inputDataTypes = {
+    name: string;
+    email: string;
+    password: string;
+    phone: string;
+    gender: string;
+    username: string;
+  };
+  /* state for taking the input values */
+  const [registerForm, setRegisterForm] = useState<inputDataTypes>({
+    name: "",
+    email: "",
+    phone: "",
+    gender: "",
+    username: "",
+    password: "",
+  });
+
+  /* picking value from input */
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRegisterForm({
+      ...registerForm,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  /* submitting the form */
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!registerForm.name) return toast.error("Name is required");
+    if (!registerForm.email) return toast.error("Email is required");
+    if (!registerForm.username) return toast.error("Username is required");
+    if (!registerForm.password) return toast.error("Password is required");
+    console.log(registerForm);
+    event.currentTarget.reset();
+    setRegisterForm({
+      name: "",
+      email: "",
+      phone: "",
+      gender: "",
+      username: "",
+      password: "",
+    });
+  };
+
   return (
     <section className="grid mt-24 place-items-center">
       <form
+        onSubmit={handleSubmit}
         action="/"
         className="sm:w-1/3 bg-slate-50 p-10 rounded-lg relative"
       >
@@ -28,6 +78,8 @@ const Register = () => {
             type="text"
             placeholder="Name here"
             className="input input-bordered w-full"
+            name="name"
+            onChange={handleChange}
           />
         </div>
         <div className="form-control w-full">
@@ -40,6 +92,8 @@ const Register = () => {
             type="email"
             placeholder="Email here"
             className="input input-bordered w-full"
+            name="email"
+            onChange={handleChange}
           />
         </div>
         <div className="form-control w-full">
@@ -49,7 +103,9 @@ const Register = () => {
           <input
             type="text"
             placeholder="Phone No here"
+            name="phone"
             className="input input-bordered w-full"
+            onChange={handleChange}
           />
         </div>
         <div className=" w-full form-control">
@@ -58,15 +114,33 @@ const Register = () => {
           </label>
           <div className="flex items-center gap-4 border border-slate-300 p-3 rounded">
             <div className="flex items-center gap-3">
-              <input type="radio" id="male" name="gender" />
+              <input
+                type="radio"
+                id="male"
+                name="gender"
+                value="male"
+                onChange={handleChange}
+              />
               <label htmlFor="male">Male</label>
             </div>
             <div className="flex items-center gap-3">
-              <input type="radio" id="female" name="gender" />
+              <input
+                type="radio"
+                id="female"
+                name="gender"
+                value="female"
+                onChange={handleChange}
+              />
               <label htmlFor="female">Female</label>
             </div>
             <div className="flex items-center gap-3">
-              <input type="radio" id="other" name="gender" />
+              <input
+                type="radio"
+                id="other"
+                value="other"
+                name="gender"
+                onChange={handleChange}
+              />
               <label htmlFor="other">Other</label>
             </div>
           </div>
@@ -80,6 +154,8 @@ const Register = () => {
           <input
             type="text"
             placeholder="Username here"
+            onChange={handleChange}
+            name="username"
             className="input input-bordered w-full"
           />
         </div>
@@ -92,6 +168,8 @@ const Register = () => {
           <input
             type="password"
             placeholder="Password here"
+            onChange={handleChange}
+            name="password"
             className="input input-bordered w-full"
           />
         </div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import store from "./Store/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <Toaster />
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
