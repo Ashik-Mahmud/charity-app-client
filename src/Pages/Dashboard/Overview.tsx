33367ad3a0587bca72c95676a1f45ceae300e7dd
ashikmahmud <ashikmamud187@gmail.com>
2022-07-21 20:22:52 +0600

    added and optimze code and also set the name in the overview page

--- src/Pages/Dashboard/Overview.tsx
+++ src/Pages/Dashboard/Overview.tsx
@@ -1,12 +1,12 @@
-import Cookies from "universal-cookie";
 import { useAppSelector } from "../../Store/store";
 const Overview = () => {
-  const cookies = new Cookies();
-  const user = useAppSelector((state) => state.loginUser);
+  const { user } = useAppSelector((state) => state.loginUser);
 
   return (
     <div className="grid place-items-center min-h-[80vh]">
-      <h3 className="text-3xl font-bold ">Welcome To the Support Dashboard</h3>
+      <h3 className="text-3xl font-bold ">
+        Welcome <b>{user?.name}</b> To the Support Dashboard
+      </h3>
     </div>
   );
 };
